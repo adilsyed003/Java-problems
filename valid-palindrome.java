63319dc5dd1a9347c2df7404f9e8f1505f94038d
Syed Adil Abraar <129985270+adilsyed003@users.noreply.github.com>
2024-07-09 22:13:40 +0530

    Update valid-palindrome.java

--- valid-palindrome.java
+++ valid-palindrome.java
@@ -1,1 +1,18 @@
-
+class Solution {
+    public boolean isPalindrome(String s) {
+         StringBuilder filtered = new StringBuilder();
+        for (char c : s.toCharArray()) {
+            if (Character.isLetterOrDigit(c)) {
+                filtered.append(Character.toLowerCase(c));
+            }
+        }
+        int j = filtered.length()-1;
+        for(int i=0;i<filtered.length();i++){
+            if(filtered.charAt(i) != filtered.charAt(j)){
+                return false;
+            }
+            j--;
+        }
+            return true;
+    }
+}
