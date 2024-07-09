efd1e9ec817174aa4dfdb7d3ff5ee937b0210d4e
Syed Adil Abraar <129985270+adilsyed003@users.noreply.github.com>
2024-07-09 22:26:16 +0530

    Update subsequence.java

--- subsequence.java
+++ subsequence.java
@@ -1,1 +1,16 @@
 
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        int slen = 0;
+        if(s.length()==0){
+            return true;
+        }
+        for(int i=0;i<t.length();i++){
+            if(s.charAt(slen)==t.charAt(i))
+                slen++;
+            if(slen == s.length())
+            return true;
+        }
+        return false;
+    }
+}
