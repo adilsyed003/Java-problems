d548fd4c7c4601b0a93c574a9fca909f6466bd13
Syed Adil Abraar <129985270+adilsyed003@users.noreply.github.com>
2024-07-10 21:35:30 +0530

    Update log-folder-d4.java

--- log-folder-d4.java
+++ log-folder-d4.java
@@ -1,1 +1,18 @@
-
+class Solution {
+    public int minOperations(String[] logs) {
+        int count = 0;
+        for(int i=0;i<logs.length;i++){
+            if(logs[i].contains("..")){
+                if(count <= 0)
+                count = 0;
+                else
+                count -= 1;
+            }else if(logs[i].contains(".")){
+                count= count;
+            }else{
+                count += 1;
+            }
+        }
+        return ((count<0)?0:count);
+    }
+}
