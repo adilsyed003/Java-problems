0e82576a3bec940605d116c445b46f16d82ac79a
Syed Adil Abraar <129985270+adilsyed003@users.noreply.github.com>
2024-07-13 22:34:44 +0530

    Update length-of-last-word.java

--- length-of-last-word.java
+++ length-of-last-word.java
@@ -1,1 +1,13 @@
-
+class Solution {
+    public int lengthOfLastWord(String s) {
+        int i=s.length()-1;
+        while(s.charAt(i) == ' ')
+        {
+        if(s.lastIndexOf(" ") == s.length()-1){
+            s = s.substring(0, s.length() - 1);
+            i--;
+        }
+        }
+        return (s.length() - s.lastIndexOf(" ")-1);
+    }
+}
