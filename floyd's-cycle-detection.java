c229e8246f433f0e17daebcb41b59eed50ec4ec6
Syed Adil Abraar <129985270+adilsyed003@users.noreply.github.com>
2024-08-16 23:34:50 +0530

    Update floyd's-cycle-detection.java

--- floyd's-cycle-detection.java
+++ floyd's-cycle-detection.java
@@ -1,1 +1,19 @@
-
+class Solution {
+        public int square(int a){
+            int sum =0;
+            while(a!=0){
+                sum += (a%10)*(a%10);
+                a /= 10;
+            }
+            return sum;
+        }
+    public boolean isHappy(int n) {
+        int slow  = n;
+        int fast = square(n);
+        while(fast!=1 && fast!=slow){
+            slow = square(slow);
+            fast = square(square(fast));
+        }
+        return fast==1;
+    }
+}
