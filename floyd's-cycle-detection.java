0ee2f9879bad0f9fd4c41d01f11b810bb10483a1
Syed Adil Abraar <129985270+adilsyed003@users.noreply.github.com>
2024-08-16 23:36:55 +0530

    Update floyd's-cycle-detection.java

--- floyd's-cycle-detection.java
+++ floyd's-cycle-detection.java
@@ -18,3 +18,31 @@ class Solution {
         return fast==1;
     }
 }
+
+
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        if(head == null || head.next == null)
+        return false;
+
+        ListNode temp = head.next;
+        while(head!=null && temp!=null && temp.next!=null){
+            head = head.next;
+            temp = temp.next.next;
+            if(head == temp) return true;
+        }
+        return false;
+    }
+}
